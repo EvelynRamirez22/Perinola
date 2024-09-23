3c63acfc509ca91f47db086bf1a4e8a5cb15630b
Evelyn Ramirez <eveeramirez92@gmail.com>
2024-09-23 12:00:47 -0300

    hicimos la pagina del juego pero me tira error en sacar ficha.

--- clases/juego.py
+++ clases/juego.py
@@ -1,18 +1,82 @@
-from perinola import Perinola
 from apuesta import Apuesta
-
-#p = Perinola()
-#print(p)
-#print(p.cara_visible)
-#resultado = p.tirar
+from perinola import Perinola
+from Jugador import Jugador
+from ronda import Ronda
 
 
-a = Apuesta()
-#print(a)
+ronda = Ronda()
+nombres = ("j1", "j2", "j3", "j4")
+for n in nombres:
+    j = Jugador(n)
+    ronda.agregarJugador(j)
+apuesta = Apuesta()
+perinola = Perinola()
 
-#a.tomarFicha(3)  
-#print(a)
-#print(a.tieneFicha(5))
-#print(a.tieneFicha(8))
-#print(a.tomarTodas())
-#print(a.estaVacia()) 
+def ponerTodos():
+    print("Ponen todos")
+    for j in ronda.jugadores:
+        j.sacarFicha()
+        apuesta.ponerFicha()    
+
+ponerTodos()
+
+while not ronda.quedaUnSoloJugador():
+    print(ronda)
+    print(apuesta)
+
+    if apuesta.estaVacia():
+        ponerTodos()
+    ronda.sacarJugadoresSinFichas()
+
+    if ronda.quedaUnSoloJugador():
+        break
+    elif len(ronda.jugadores) == 0:
+        break
+
+    jt = ronda.jugadorEnTurno()
+    print("le toca a: ", end="")
+    print(jt)
+
+    p = perinola.tirar()
+    print(p)
+
+    if p == "Pon 1":
+        jt.sacarFicha()
+        apuesta.ponerFicha()
+    elif p == "Pon 2":
+        if jt.fichas == 1:
+            jt.sacarFicha()
+            apuesta.ponerFicha()
+        else:
+            jt.sacarFicha(2)
+            apuesta.ponerFicha(2)
+    elif p == "Toma 1":
+        apuesta.tomarFicha()
+        jt.darFicha()
+    elif p == "Toma 2":
+        if apuesta.fichas == 1:
+            apuesta.tomarFicha()
+            jt.darFicha()
+        else:
+            apuesta.tomarFicha(2)
+            jt.darFicha(2)
+    elif p == "Toma Todo":
+        fichas = apuesta.tomarTodas()
+        jt.darFicha(fichas)
+    elif p == "Todos Ponen":
+        ponerTodos()
+    else:
+        raise ValueError("Valor inesperado")
+
+
+
+    print(apuesta)
+    print(ronda)
+    ronda.sacarJugadoresSinFichas()
+    ronda.pasarTurno()
+    print()
+
+if ronda.quedaUnSoloJugador():
+    print(f"Terminó el juego, ganó: {ronda.jugadorEnTurno()}")
+else:
+    print(f"Terminó el juego, perdieron todos")
--- clases/ronda.py
+++ clases/ronda.py
@@ -1,30 +1,33 @@
 class Ronda:
+
     def __init__(self):
-        self.jugadores = []
+        self.jugadores = [0]
+
+    def __str__(self):
+        return "\n".join(str(jugador) for jugador in self.jugadores)
 
     def agregarJugador(self, jugador):
-        if jugador.fichas <= 0:
-            raise ValueError("El jugador no puede tener 0 o menos fichas.")
-        self.jugadores.append(jugador)
+        if jugador.fichas > 0:
+            self.jugadores.append(jugador)
+        else:
+            raise ValueError("El jugador debe tener fichas para ser agregado.")
 
     def sacarJugadoresSinFichas(self):
-        self.jugadores = [jugador for jugador in self.jugadores if jugador.fichas > 0]
+        self.jugadores = [jugador for jugador in self.jugadores if not jugador.sinFichas()]
 
     def jugadorEnTurno(self):
-        if not self.jugadores:
+        if self.jugadores:
+            return self.jugadores[0]
+        else:
             raise IndexError("No hay jugadores en la ronda.")
-        return self.jugadores[0]
 
     def pasarTurno(self):
-        if not self.jugadores:
+        if self.jugadores:
+            jugador = self.jugadores.pop(0)
+            self.jugadores.append(jugador)
+        else:
             raise IndexError("No hay jugadores en la ronda.")
-        jugador = self.jugadores.pop(0)
-        self.jugadores.append(jugador)
 
     def quedaUnSoloJugador(self):
         return len(self.jugadores) == 1
 
-    def __str__(self):
-        if not self.jugadores:
-            return "No hay jugadores en la ronda."
-        return "\n".join(str(jugador) for jugador in self.jugadores)
